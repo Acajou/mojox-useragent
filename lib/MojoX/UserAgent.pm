fb4d5a20e09f1c03e355509402ec40f5a7157a9b
Acajou <acajou@gmail.com>
2009-07-29 16:47:13 -0400

    Basic redirect support...

--- lib/MojoX/UserAgent.pm
+++ lib/MojoX/UserAgent.pm
@@ -15,14 +15,13 @@ use Mojo::Client;
 use Mojo::Cookie;
 
 __PACKAGE__->attr('redirect_limit', default => 10);
-# __PACKAGE__->attr('_client', default => sub { Mojo::Client->new });
-# __PACKAGE__->attr('_handler', default => undefined);
+__PACKAGE__->attr('_client',  default => sub { Mojo::Client->new });
 
 our $VERSION = '0.001';
 
 sub new {
     my $self = shift->SUPER::new();
-    $self->{_client} = Mojo::Client->new;
+    # $self->{_client} = Mojo::Client->new;
     return $self;
 }
 
@@ -44,19 +43,42 @@ sub run_all {
 
 sub crank {
     my $self = shift;
-    my @transactions = @{$self->{_txs}};
-    $self->{_client}->spin(@transactions);
+    my $transactions = $self->{_txs};
+    $self->_client->spin(@{$transactions});
     my @buffer;
-    while (my $tx = shift @transactions) {
+    while (my $tx = shift @{$transactions}) {
         if ($tx->is_finished) {
-            # Callback
-            print $tx->req->url . " done!\n";
-        } else {
+            if ($tx->res->is_status_class(300)
+                && (!$tx->{_hops} || $tx->{_hops} < $self->redirect_limit)
+                && (my $location = $tx->res->headers->header('Location')))
+            {
+
+                # Presumably 304 (not modified) shouldn't include
+                # a Location so shouldn't come in here...
+
+                unless ($tx->res->code == 305) {
+                    my $new_tx = Mojo::Transaction->new_get($location);
+                    $new_tx->{_hops} = $tx->{_hops} ? $tx->{_hops}+1 : 1;
+                    $self->spool_tx($new_tx);
+                }
+                else {
+
+                    # Set up a proxied request (TODO)
+                }
+            }
+            else {
+
+                # Callback (TODO)
+                print $tx->req->url . " done!\n";
+                print "Hops: " . ($tx->{_hops} ? $tx->{_hops} : 0) . "\n";
+            }
+        }
+        else {
             push @buffer, $tx;
         }
     }
-    push @transactions, @buffer;
-    return scalar @transactions;
+    push @{$transactions}, @buffer;
+    return scalar @{$transactions};
 }
 
 sub handler {
--- t/basic.t
+++ t/basic.t
@@ -17,3 +17,9 @@ my $tx1 =  Mojo::Transaction->new_get('http://127.0.0.1:3000/');
 $ua->spool_tx($tx1);
 
 $ua->run_all;
+
+my $tx2  = Mojo::Transaction->new_get('http://labs.kraih.com');
+
+$ua->spool_tx($tx2);
+
+$ua->run_all;
